3b4c2e120b9addf412aaa0a5a2407db87ac30246
glayberson <glaybersonsilva@outlook.com>
2026-01-28 20:04:56 -0300

    add: finalizando MLP e add keras

--- Rede_mlp.py
+++ Rede_mlp.py
@@ -25,3 +25,30 @@ np.save('classes.npy', classes)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y_encoded, test_size=0.2, random_state=42
 )
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(63,)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(len(classes), activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+print("Iniciando treinamento da MLP")
+history = model.fit(
+    X_train,
+    y_train,
+    epochs=50,
+    batch_size=32,
+    validation_data=(X_test, y_test)
+)
+
+model.save('modelo_mlp.keras')
+print("Modelo salvo como 'modelo_mlp.keras'")
